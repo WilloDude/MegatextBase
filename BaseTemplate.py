26647e47e29acd1d5b3c391a160ec1562612c054
WilloDude <87053373+WilloDude@users.noreply.github.com>
2022-06-11 19:56:19 +1000

    Delete BaseTemplate from BoxFace
    New BaseTemplate file has been made and this one was merged from BoxFace, there is no need.

--- BaseTemplate.py
+++
@@ -1,8 +0,0 @@
-start1= input("<Input Art Name Here> start? ")
-print("Made with Megatext") ## Feel Free to remove this! I understand the pain of watermarks and other marketting things!
-## Input Code Here
-print("")
-## Ending Sequence
-end1= input("Art made by <Input Name Here>")
-print("Made with Megatext") ## Feel Free to remove this! I understand the pain of watermarks and other marketting things!
-end2= input("... ") ## This is to stop the file from automatically closing in the python console
